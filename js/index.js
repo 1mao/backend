6ac3a6144934fd7a5725b76e90ddafe62e97c765
wanghongxin492 <wanghongxin492@emao.com>
2015-10-19 19:17:03 +0800

    init

---
+++ js/index.js
@@ -0,0 +1,20 @@
+window.onload=function(){
+	var scroll=new IScroll('#scroll_zone',{ 
+		scrollX: true, 
+		scrollY: false, 
+		mouseWheel: true 
+	});
+	
+	var date_start=new JsDatePick({
+			useMode:2,
+			target:"data_start",
+			dateFormat:"%Y-%m-%d"
+		});
+	var date_end=new JsDatePick({
+		useMode:2,
+		target:"data_end",
+		dateFormat:"%Y-%m-%d"
+	});
+};
+
+
